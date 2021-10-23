87ed934f6f2ca40fae28dff41c4bdb9df354f7a4
Prashant Joshi <scienceprashant27@gmail.com>
2021-10-23 16:49:23 +0530

    Added frontend javascript

---
+++ public/main.js
@@ -0,0 +1,42 @@
+const fetchWeather = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+  );
+
+  if (response.status == 404) {
+    alert("Please enter a vaild city");
+  }
+
+  if ((response.status = 200)) {
+    const result = await response.json();
+
+    const data = {
+      city: result.name,
+      temp: result.main.temp,
+    };
+    addWeatherToDom(data);
+  }
+};
+
+const weatherDisplay = document.querySelector("#weather");
+const addWeatherToDom = (data) => {
+  weatherDisplay.innerHTML = `
+        <h2>Weather in ${data.city}</h2>
+        <h1>${data.temp}</h1>
+    `;
+};
+
+const weatherForm = document.querySelector("#weather-form");
+const cityInput = document.querySelector("#city-input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (cityInput.value === "") {
+    alert("Please enter a city");
+  } else {
+    fetchWeather(cityInput.value);
+  }
+});
+
+fetchWeather("Delhi");
